949d64f13322ec0e21e18beeb5e7d8267681a4d6
imnikhilanand <imnikhilanand@gmail.com>
2023-04-25 14:52:07 -0400

    Added models

--- modeling.py
+++ modeling.py
@@ -11,6 +11,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
+import xgboost as xgb
 
 # reading the data
 data = pd.read_csv("dataset/african_crises.csv")
@@ -37,17 +43,41 @@ data_train = data.iloc[:,3:13]
 # concatenating the two dataframe
 result = pd.concat([data_train, onehot_country_df], axis=1)
 
+# train-test split in dataframe
+X = result.values
+y = enc_crisis
 
+# handling the imbalancing of the class
+oversample = SMOTE()
+X, y = oversample.fit_resample(X, y)
 
+# splitting the dataset
+X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                    y, 
+                                                    test_size=0.2, 
+                                                    random_state=0, 
+                                                    stratify=y)
 
+# Logistic Regression
+clf = LogisticRegression(random_state=0).fit(X_train, y_train)
+y_pred = clf.predict(X_test)
 
+# Logistic Regression results 
+print(classification_report(y_test, y_pred))
 
+# Random Forest Classification
+clf = RandomForestClassifier(max_depth=2, random_state=0).fit(X_train, y_train)
+y_pred = clf.predict(X_test)
 
+# Random Forest results 
+print(classification_report(y_test, y_pred))
 
+# XGBoost Classification
+model = xgb.XGBClassifier().fit(X_train, y_train)
+y_pred = model.predict(X_test)
 
-
-
-
+# XGBoost results 
+print(classification_report(y_test, y_pred))
 
 
 
